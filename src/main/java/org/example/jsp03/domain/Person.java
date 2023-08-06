65b7686632b712bb6b979c8d1d48d6fb63e8524c
fouad <133219919+mhoseinre@users.noreply.github.com>
2023-08-06 13:16:54 +0330

    commit

--- src/main/java/org/example/jsp03/domain/Person.java
+++ src/main/java/org/example/jsp03/domain/Person.java
@@ -1,4 +1,37 @@
 package org.example.jsp03.domain;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = Person.TABLE_NAME)
 public class Person {
+    public static final String TABLE_NAME="person";
+    public static final String FARST_NAME="firstName";
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = FARST_NAME)
+    private String farstName;
+
+    public String getFarstName() {
+        return farstName;
+    }
+    public void setFarstName(String farstName) {
+        this.farstName = farstName;
+    }
+
+
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Person() {
+    }
+
 }
